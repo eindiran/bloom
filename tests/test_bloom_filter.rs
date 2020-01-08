e8b1bf7f6b61bf93c41c7ee8cd9eb4063831a950
Elliott Indiran <eindiran@users.noreply.github.com>
2020-01-08 15:31:49 -0800

    Update test_bloom_filter.rs to use standard Rust docstrings

--- tests/test_bloom_filter.rs
+++ tests/test_bloom_filter.rs
@@ -7,36 +7,36 @@
 extern crate bloom;
 
 #[test]
+/// Test that the getter for hash_count is visible from outside the crate
 fn test_get_hash_count() {
-    // Test that the getter for hash_count is visible from outside the crate
     let bf: bloom::BloomFilter = bloom::BloomFilter::new(10000, 0.01);
     assert!(bf.get_hash_count() > 0);
 }
 
 #[test]
+/// Test that the getter for false_positive_rate is visible from outside the crate
 fn test_get_false_positive_rate() {
-    // Test that the getter for false_positive_rate is visible from outside the crate
     let bf: bloom::BloomFilter = bloom::BloomFilter::new(10000, 0.01);
     assert_eq!(bf.get_false_positive_rate(), 0.01);
 }
 
 #[test]
+/// Test that the getter for expected_inserts is visible from outside the crate
 fn test_get_expected_inserts() {
-    // Test that the getter for expected_inserts is visible from outside the crate
     let bf: bloom::BloomFilter = bloom::BloomFilter::new(10000, 0.01);
     assert_eq!(bf.get_expected_inserts(), 10000);
 }
 
 #[test]
+/// Test that the getter for actual_inserts is visible from outside the crate
 fn test_get_actual_inserts() {
-    // Test that the getter for actual_inserts is visible from outside the crate
     let bf: bloom::BloomFilter = bloom::BloomFilter::new(10000, 0.01);
     assert_eq!(bf.get_actual_inserts(), 0);
 }
 
 #[test]
+/// Ensure that the false positive rate is close to the actual value
 fn test_false_positive_rate() {
-    // Ensure that the false positive rate is close to the actual value
     let mut bf: bloom::BloomFilter = bloom::BloomFilter::new(10000, 0.01);
     for i in 0..10000 {
         bf.insert(&i.to_string());
