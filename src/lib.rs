cf4225c5a10800e624ab64a19745e10b2af8d5c4
eindiran <eindiran@uchicago.edu>
2020-01-24 17:16:27 -0800

    Add the current list of supported classes to lib.rs header

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,16 @@
  * lib.rs
  *
  * Implements a Murmur3-based bloom filter variations:
- *      BloomFilter             -- a standard bloom filter.
- *      CountingBloomFilter     -- a bloom filter which supports deleting items.
- *      ScopeDecayBloomFilter   -- a scope decay bloom filter, supporting gradual resetting of bits
- *                                 over time.
- *      GenerationalBloomFilter -- use 2 bloom filters to support a moving window of data.
+ *      BloomFilter               -- a standard bloom filter.
+ *      CountingBloomFilter       -- a bloom filter which supports deleting items, using counters
+ *                                   instead of bits.
+ *      ScopeDecayBloomFilter     -- a scope decay bloom filter, supporting gradual resetting of bits
+ *                                   over time.
+ *      GenerationalBloomFilter   -- use N generations of bloom filters to support a moving window of data.
+ *      BigenerationalBloomFilter -- use 2 alternating generations of bloom filters to support a
+ *                                   moving window of data.
+ *      A2BufferingBloomFilter    -- use a segmented bloom filter, relying on the active-active
+ *                                   buffering strategy to keep recent elements in the current
+ *                                   segment.
  */
 pub mod bloom; // Export the module defined in bloom.rs
